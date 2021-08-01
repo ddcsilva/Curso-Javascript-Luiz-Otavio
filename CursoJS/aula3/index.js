af35b475071f0ca8622263a455d9ff051c3c9fa5
Danilo Silva <danilo.silva@msn.com>
2021-07-31 23:12:14 -0300

    Aula - Console.log

---
+++ CursoJS/aula3/index.js
@@ -0,0 +1,14 @@
+// String - Aspas Simples (Comum)
+console.log('Danilo Silva');
+
+// String - Aspas Duplas
+console.log("Danilo Silva");
+
+// String - Crase (Template String)
+console.log(`Danilo Silva`);
+
+// Inteiro
+console.log(35);
+
+// Ponto Flutuante
+console.log(15.85);
