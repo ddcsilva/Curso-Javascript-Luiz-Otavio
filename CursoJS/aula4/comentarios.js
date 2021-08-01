0c98fb48d97eb74f4034cb28f134df312a2f1811
Danilo Silva <danilo.silva@msn.com>
2021-07-31 23:23:47 -0300

    Aula - Comentários de Código

---
+++ CursoJS/aula4/comentarios.js
@@ -0,0 +1,14 @@
+// Um comentário
+console.log('Olá mundo!'); // Outro comentário
+console.log('Linha 1');
+// Comentário
+
+/**
+ * Um bloco de código
+ * Linha de comando
+ */
+
+console.log('Linha 2');
+//console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
