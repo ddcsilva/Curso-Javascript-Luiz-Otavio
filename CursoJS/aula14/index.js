5f368419957fcf7726f8e4c7455a0d76b1abe4c7
Danilo Silva <danilo.silva@msn.com>
2021-08-01 17:54:26 -0300

    Aula - Mais Sobre Numbers

---
+++ CursoJS/aula14/index.js
@@ -0,0 +1,16 @@
+let num1 = 1;
+let num2 = 2.5;
+
+console.log(num1.toString() + num2);
+
+console.log(num2.toString(2));
+
+console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+
+num1 = 10.63948574839234546;
+
+console.log(num1.toFixed(2));
+
+let teste = num1 * 'Danilo';
+console.log(Number.isNaN(teste));
