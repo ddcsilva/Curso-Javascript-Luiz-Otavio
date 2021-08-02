dd83487f779744cfe8a4d00d8300f5d37a568c02
Danilo Silva <danilo.silva@msn.com>
2021-08-02 20:41:46 -0300

    Aula - Operadores Lógicos

---
+++ CursoJS/aula22/index.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores Lógicos
+ * && -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar TRUE
+ * || -> OR  -> OU
+ * !  -> NOT -> NÃO
+ */
+
+console.log(true && true);
+console.log(true && true && true && false);
+
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+console.log(true || false || true || false);
+
+console.log(!true)
+console.log(!false)
