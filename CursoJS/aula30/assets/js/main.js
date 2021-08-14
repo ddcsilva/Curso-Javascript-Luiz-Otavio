9ec18622654d4a0bc94530d1d2e9193a3fbed0e2
Danilo Silva <danilo.silva@msn.com>
2021-08-14 00:44:45 -0300

    Update main.js

--- CursoJS/aula30/assets/js/main.js
+++ CursoJS/aula30/assets/js/main.js
@@ -2,11 +2,11 @@ const container = document.querySelector('.container');
 const h1 = container.querySelector('h1');
 const today = new Date();
 
-const dayName = new Array ('domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado');
-const monthName = new Array ('janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro');
+const dayName = new Array('domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado');
+const monthName = new Array('janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro');
 
-function zeroAEsquerda (num) {
-    return num >= 10 ? num : `0${num}`;
-  }
+function zeroAEsquerda(num) {
+  return num >= 10 ? num : `0${num}`;
+}
 
 h1.innerHTML = `${dayName[today.getDay()]}, ${today.getDate()} de ${monthName[today.getMonth()]} de ${today.getFullYear()} ${zeroAEsquerda(today.getHours())}:${zeroAEsquerda(today.getMinutes())}`;
